8b8c2242684e6adba42f3810020c2443d3d5caf9
Tom Kazimiers <tom@voodoo-arts.net>
2014-02-12 11:51:29 -0500

    Djsopnet: Add front-end for test of segment guarantor task

--- django/applications/djsopnet/static/js/controllers.js
+++ django/applications/djsopnet/static/js/controllers.js
@@ -3,8 +3,22 @@
 
   controllers = angular.module('sopnetApp.controllers', []);
 
-  controllers.controller('overviewController', function($scope, $state, $log, tasks) {
-    return $scope.tasks = tasks.all;
-  });
+  controllers.controller('overviewController',
+      function($scope, $state, $log, $http, tasks) {
+        $scope.launchSegmentGuarantorTask = function() {
+          $log.info("Launching Segment Guarantor Task");
+          return $http({
+            method: 'GET',
+            url: 'segmentguarantor/test',
+          }).success(function(data) {
+            return $log.info("Successfully launched Segment Guarantor Task.");
+          }).error(function(data) {
+            return $log.info("Failed to launch Segment Guarantor Task.");
+          });
+        };
+
+        $scope.tasks = tasks.all;
+        return
+      });
 
 }).call(this);
