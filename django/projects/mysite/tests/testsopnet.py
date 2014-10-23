8dc5b52c06ff8f3b4264ecb57a7d8d7f701aa231
Andrew Champion <andrew.champion@gmail.com>
2014-10-22 21:12:10 -0400

    Djsopnet: set feature weights for test dataset
    Set default feature weights for test dataset. Keep create_testdata from
    duplicating test datasets (this may not always be desirable behavior,
    but as a scaffold it is for now). Create a dummy name for all features.

--- django/projects/mysite/tests/testsopnet.py
+++ django/projects/mysite/tests/testsopnet.py
@@ -7,6 +7,7 @@ from catmaid.fields import Double3D, Integer3D
 
 from django.conf import settings
 
+from djsopnet.models import FeatureName, FeatureInfo
 from djsopnet.views import create_project_config, _clear_djsopnet
 from djsopnet.views import _setup_blocks
 import pysopnet as ps
@@ -19,34 +20,48 @@ def print_locations(locations):
 
 def create_testdata():
 	# Create test stack for raw data
-	sr = Stack(title="Catsop Test Raw",
-		image_base='http://neurocity.janelia.org/catsop/data/catsop_test_raw/',
-		resolution=Double3D(4.0, 4.0, 4.0),
-		dimension=Integer3D(1024, 1024, 20),
-		tile_height=1024,
-		tile_width=1024,
-		file_extension='png')
-	sr.save()
+	sr, created = Stack.objects.get_or_create(title="Catsop Test Raw",
+		defaults={'image_base':'http://neurocity.janelia.org/catsop/data/catsop_test_raw/',
+			'resolution':Double3D(4.0, 4.0, 4.0),
+			'dimension':Integer3D(1024, 1024, 20),
+			'tile_height':1024,
+			'tile_width':1024,
+			'file_extension':'png'})
 
 	# Create test stack for membrane data
-	sm = Stack(title="Catsop Test Membrane",
-		resolution=Double3D(4.0, 4.0, 4.0),
-		dimension=Integer3D(1024, 1024, 20),
-		image_base='http://neurocity.janelia.org/catsop/data/catsop_test_membrane/',
-		tile_height=1024,
-		tile_width=1024,		
-		file_extension='png')
-	sm.save()
+	sm, created = Stack.objects.get_or_create(title="Catsop Test Membrane",
+		defaults={'image_base':'http://neurocity.janelia.org/catsop/data/catsop_test_raw/',
+			'resolution':Double3D(4.0, 4.0, 4.0),
+			'dimension':Integer3D(1024, 1024, 20),
+			'tile_height':1024,
+			'tile_width':1024,
+			'file_extension':'png'})
 
 	# Create new test project
-	p = Project(title="Catsop Test")
-	p.save()
+	p, created = Project.objects.get_or_create(title="Catsop Test")
 
 	# Link both stacks to project
-	psr = ProjectStack(project=p, stack=sr)
-	psr.save()
-	psm = ProjectStack(project=p, stack=sm)
-	psm.save()
+	psr, created = ProjectStack.objects.get_or_create(project=p, stack=sr)
+	psm, created = ProjectStack.objects.get_or_create(project=p, stack=sm)
+
+	featureWeights = \
+		[0.00558637,-1.6678e-05,0.00204453,0.0711393,-0.00135737,3.35817,
+		-0.000916876,-0.000957261,-0.00193582,-1.48732,-0.000234868,-4.21938,
+		0.501363,0.0665533,-0.292248,0.0361189,0.0844144,-0.0316035,0.0127795,
+		-0.00765338,-0.00558571,-0.0172858,0.00562492,-0.0109868,-0.00136111,
+		-0.0227562,-0.0825309,-0.131062,-0.442795,0.354401,0.266398,1.46761,
+		-0.743354,-0.281164,0.169887,0.262849,-0.0505789,0.00516085,0.0138543,
+		-0.0102862,0.0080712,0.00012668,-0.0031432,0.00186596,0.00371999,
+		-0.0688746,0.324525,0.79521,1.88847,2.09861,1.51523,0.394032,0.477188,
+		-0.0952926,0.374847,0.253683,0.840265,-2.89614,4.2625e-10]
+	fi, created = FeatureInfo.objects.get_or_create(stack=sr,
+		defaults={'size':len(featureWeights), 'name_ids':[0], 'weights':featureWeights})
+	if created:
+		unnamedFeature = FeatureName(name='Unnamed Feature')
+		unnamedFeature.save()
+		featureNames = [unnamedFeature.id for i in range(len(featureWeights))]
+		fi.name_ids = featureNames
+		fi.save()
 
 	print("SOPNET_PROJECT_ID = %s" % p.id)
 	print("SOPNET_RAW_STACK_ID = %s" % sr.id)
