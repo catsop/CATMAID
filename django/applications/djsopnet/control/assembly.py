b54d9722176ae33f1bcb9f3c70923c1e7a1d9e38
Andrew Champion <andrew.champion@gmail.com>
2015-02-09 14:22:53 -0500

    Djsopnet: store assembly conflicts and continuations
    Factor assembly compatibility query to query and store continuation
    conflicts and continuations in the AssemblyRelation model. Compatible
    assemblies can now be queried directly from these relationships.

--- django/applications/djsopnet/control/assembly.py
+++ django/applications/djsopnet/control/assembly.py
@@ -7,31 +7,40 @@ def find_compatible_assemblies_between_cores(core_a_id, core_b_id):
     """Returns an array of assembly ID tuples, where for each tuple the
     members are assembly IDs for precedent solutions from core_a and core_b,
     respectively, and the two assemblies in the tuple have at least one
-    continuation and no conflicts. A continuation is defined to be a shared
-    slice. A conflict between assemblies is defined to be the existence of a
+    continuation and no conflicts.
+
+    Continuations and conflicts for this core pair should be generated first.
+    """
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT ar.assembly_a_id, ar.assembly_b_id, array_agg(ar.relation)
+        FROM djsopnet_assemblyrelation ar
+        JOIN djsopnet_segmentsolution ssol ON ssol.id = (
+            SELECT id FROM djsopnet_segmentsolution
+            WHERE assembly_id = ar.assembly_a_id
+              OR assembly_id = ar.assembly_b_id
+            LIMIT 1)
+        JOIN djsopnet_solutionprecedence sp ON sp.solution_id = ssol.solution_id
+        WHERE sp.core_id = %s OR sp.core_id = %s
+        GROUP BY ar.assembly_a_id, ar.assembly_b_id
+        HAVING 'Continuation' = ALL(array_agg(ar.relation))
+        """ % (core_a_id, core_b_id))
+    compatible = frozenset(cursor.fetchall())
+
+    return compatible
+
+def generate_conflicting_assemblies_between_cores(core_a_id, core_b_id):
+    """Create relations for conflicting precedent assemblies between cores.
+
+    A conflict between assemblies is defined to be the existence of a
     conflict edge between slices in each assembly, OR that each assembly
     involves the same slice but via different segments in the same section. That
     is, conflicting assemblies contain conflicting slices or exclusive
-    segments."""
-
-    cursor = connection.cursor()
-    cursor.execute("""
-        SELECT DISTINCT ssol1.assembly_id, ssol2.assembly_id
-        FROM djsopnet_solutionprecedence sp1
-        JOIN djsopnet_segmentsolution ssol1
-          ON (ssol1.solution_id = sp1.solution_id AND ssol1.assembly_id IS NOT NULL)
-        JOIN djsopnet_segmentslice ss1 ON ss1.segment_id = ssol1.segment_id
-        JOIN djsopnet_segmentslice ss2
-          ON (ss2.slice_id = ss1.slice_id AND ss2.id <> ss1.id)
-        JOIN djsopnet_segmentsolution ssol2
-          ON (ssol2.segment_id = ss2.segment_id AND ssol2.assembly_id IS NOT NULL)
-        JOIN djsopnet_solutionprecedence sp2 ON sp2.solution_id = ssol2.solution_id
-        WHERE sp1.core_id = %s AND sp2.core_id = %s
-        """ % (core_a_id, core_b_id))
-    assembly_continuations = frozenset(cursor.fetchall())
-
-    cursor.execute("""
-        SELECT DISTINCT ssol1.assembly_id, ssol2.assembly_id
+    segments.
+    """
+    # Find conflicts and concurrency-safe upsert to assembly relations.
+    conflict_query = """
+        SELECT DISTINCT ssol1.assembly_id, ssol2.assembly_id, 'Conflict'::assemblyrelation
         FROM djsopnet_solutionprecedence sp1
         JOIN djsopnet_segmentsolution ssol1
           ON (ssol1.solution_id = sp1.solution_id AND ssol1.assembly_id IS NOT NULL)
@@ -47,8 +56,55 @@ def find_compatible_assemblies_between_cores(core_a_id, core_b_id):
         JOIN djsopnet_segmentsolution ssol2
           ON (ssol2.segment_id = ss2.segment_id AND ssol2.assembly_id IS NOT NULL)
         JOIN djsopnet_solutionprecedence sp2 ON sp2.solution_id = ssol2.solution_id
-        WHERE sp1.core_id = %s AND sp2.core_id = %s
-        """ % (core_a_id, core_b_id))
-    assembly_conflicts = frozenset(cursor.fetchall())
+        WHERE sp1.core_id = %(core_a_id)s AND sp2.core_id = %(core_b_id)s;
+        """ % {'core_a_id': core_a_id, 'core_b_id': core_b_id}
+    _generate_assembly_relation_between_cores(core_a_id, core_b_id, conflict_query)
 
-    return assembly_continuations - assembly_conflicts
+def generate_continuing_assemblies_between_cores(core_a_id, core_b_id):
+    """Create relations for continuing precedent assemblies between cores.
+
+    A continuation is defined to be a shared slice.
+    """
+    # Find continuations and concurrency-safe upsert to assembly relations.
+    continuation_query = """
+        SELECT DISTINCT ssol1.assembly_id, ssol2.assembly_id, 'Continuation'::assemblyrelation
+        FROM djsopnet_solutionprecedence sp1
+        JOIN djsopnet_segmentsolution ssol1
+          ON (ssol1.solution_id = sp1.solution_id AND ssol1.assembly_id IS NOT NULL)
+        JOIN djsopnet_segmentslice ss1 ON ss1.segment_id = ssol1.segment_id
+        JOIN djsopnet_segmentslice ss2
+          ON (ss2.slice_id = ss1.slice_id AND ss2.id <> ss1.id)
+        JOIN djsopnet_segmentsolution ssol2
+          ON (ssol2.segment_id = ss2.segment_id AND ssol2.assembly_id IS NOT NULL)
+        JOIN djsopnet_solutionprecedence sp2 ON sp2.solution_id = ssol2.solution_id
+        WHERE sp1.core_id = %(core_a_id)s AND sp2.core_id = %(core_b_id)s;
+        """ % {'core_a_id': core_a_id, 'core_b_id': core_b_id}
+    _generate_assembly_relation_between_cores(core_a_id, core_b_id, continuation_query)
+
+def _generate_assembly_relation_between_cores(core_a_id, core_b_id, relationship_query):
+    cursor = connection.cursor()
+    tmp_table_name = "djsopnet_assemblyrelation_tmp_%s_%s" % (core_a_id, core_b_id)
+    # Find relationships and concurrency-safe upsert to assembly relations.
+    cursor.execute("""
+        BEGIN;
+
+        CREATE TEMP TABLE %(tmp_table_name)s
+          (LIKE djsopnet_assemblyrelation) ON COMMIT DROP;
+        ALTER TABLE %(tmp_table_name)s DROP COLUMN id;
+
+        INSERT INTO %(tmp_table_name)s (assembly_a_id, assembly_b_id, relation)
+        %(relationship_query)s
+
+        LOCK TABLE djsopnet_assemblyrelation IN EXCLUSIVE MODE;
+
+        INSERT INTO djsopnet_assemblyrelation
+          (assembly_a_id, assembly_b_id, relation)
+        SELECT t.assembly_a_id, t.assembly_b_id, t.relation
+        FROM %(tmp_table_name)s AS t
+        LEFT OUTER JOIN djsopnet_assemblyrelation a
+          ON ((a.assembly_a_id, a.assembly_b_id, a.relation) =
+              (t.assembly_a_id, t.assembly_b_id, t.relation))
+        WHERE a.id IS NULL;
+
+        COMMIT;
+        """ % {'relationship_query': relationship_query, 'tmp_table_name': tmp_table_name})
