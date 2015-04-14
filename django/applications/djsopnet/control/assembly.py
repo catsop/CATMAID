catsop/CATMAID

commit ed8321a74dddf13eab138c12cab2b583e6e3dfa2

Assemblies: improve assembly relation deletion
Use a with CTE to improve the performance of deleting existing assembly
relations when generating an assembly relation. Results in 4x speedup
for the small default test stack.

django/applications/djsopnet/control/assembly.py
@@ -366,18 +366,15 @@ def _generate_assembly_relation_between_cores(segstack_id, core_a_id, core_b_id,
 
         LOCK TABLE segstack_%(segstack_id)s.assembly_relation IN EXCLUSIVE MODE;
 
+        WITH core_assemblies AS (
+            SELECT a.id AS assembly_id
+            FROM segstack_%(segstack_id)s.assembly a
+            JOIN segstack_%(segstack_id)s.solution s ON a.solution_id = s.id
+            WHERE core_id IN (%(core_a_id)s, %(core_b_id)s))
         DELETE FROM segstack_%(segstack_id)s.assembly_relation ar
         WHERE relation = '%(relation)s'::assemblyrelation
-          AND EXISTS (
-            SELECT 1 FROM segstack_%(segstack_id)s.solution s
-            JOIN segstack_%(segstack_id)s.assembly a
-              ON (a.solution_id = s.id AND a.id = ar.assembly_a_id)
-            WHERE s.core_id = %(core_a_id)s OR s.core_id = %(core_b_id)s)
-          AND EXISTS (
-            SELECT 1 FROM segstack_%(segstack_id)s.solution s
-            JOIN segstack_%(segstack_id)s.assembly a
-              ON (a.solution_id = s.id AND a.id = ar.assembly_b_id)
-            WHERE s.core_id = %(core_a_id)s OR s.core_id = %(core_b_id)s);
+          AND (ar.assembly_a_id IN (SELECT assembly_id FROM core_assemblies) OR
+               ar.assembly_b_id IN (SELECT assembly_id FROM core_assemblies));
 
         INSERT INTO segstack_%(segstack_id)s.assembly_relation
           (assembly_a_id, assembly_b_id, relation)

commit 965af57b4befe1bb3f33ea0fab4d986d1b96caee

Assemblies: fix deletion of relations
Fix a bug introduced by ed8321a: relations involving both, not either,
of the cores in the pair should be deleted.

django/applications/djsopnet/control/assembly.py
@@ -376,7 +376,7 @@ def _generate_assembly_relation_between_cores(segstack_id, core_a_id, core_b_id,
             WHERE core_id IN (%(core_a_id)s, %(core_b_id)s))
         DELETE FROM segstack_%(segstack_id)s.assembly_relation ar
         WHERE relation = '%(relation)s'::assemblyrelation
-          AND (ar.assembly_a_id IN (SELECT assembly_id FROM core_assemblies) OR
+          AND (ar.assembly_a_id IN (SELECT assembly_id FROM core_assemblies) AND
                ar.assembly_b_id IN (SELECT assembly_id FROM core_assemblies));
 
         INSERT INTO segstack_%(segstack_id)s.assembly_relation