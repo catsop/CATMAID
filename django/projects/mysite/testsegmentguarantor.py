5f607bcde8c0dc7a346d36dbf3be4f1549e5c922
Andrew Champion <andrew.champion@gmail.com>
2015-05-15 11:10:22 -0400

    Djsopnet: add parallel harness to test scripts
    Allow the guarantor test scripts to run jobs in parallel. The scripts
    remain duplicated rather than abstracted into a single harness test
    script because it is convenient to manipulate one guarantor independent
    of the others during development.

--- django/projects/mysite/testsegmentguarantor.py
+++ django/projects/mysite/testsegmentguarantor.py
@@ -8,6 +8,11 @@ from djsopnet.control.block import _setup_blocks
 from tests.testsopnet import SopnetTest, print_locations
 import pysopnet as ps
 
+USE_PARALLEL = False
+
+if USE_PARALLEL:
+	from joblib import Parallel, delayed
+
 # Setup Sopnet environment
 st = SopnetTest()
 st.clear_database(clear_slices=False, clear_segments=True)
@@ -22,13 +27,26 @@ segmentGuarantor = ps.SegmentGuarantor()
 
 segmentGuarantorParameters = ps.SegmentGuarantorParameters()
 
+jobs = []
+
+def fill_block(x, y, z):
+	request = ps.point3(x, y, z)
+
+	print "Issuing first request for block (%s,%s,%s)" % (x, y, z)
+
+	missing = segmentGuarantor.fill(request, segmentGuarantorParameters, config)
+
+	if len(missing) > 0:
+		raise "There are (at least) the following segments missing: " + str(missing)
+
 bi = BlockInfo.objects.get(configuration_id=st.segmentation_configuration_id)
 for i in range(0, bi.num_x):
 	for j in range(0, bi.num_y):
 		for k in range(0, bi.num_z):
+			if USE_PARALLEL:
+				jobs.append(delayed(fill_block)(i, j, k))
+			else:
+				fill_block(i, j, k)
 
-			request = ps.point3(i,j,k)
-
-			print "Issuing first request for block (%s,%s,%s)" % (request.x,request.y,request.z)
-
-			missing = segmentGuarantor.fill(request, segmentGuarantorParameters, config)
+if USE_PARALLEL:
+	Parallel(n_jobs=8)(jobs)
--- django/projects/mysite/testsolutionguarantor.py
+++ django/projects/mysite/testsolutionguarantor.py
@@ -8,8 +8,14 @@ from djsopnet.control.block import _setup_blocks
 from tests.testsopnet import SopnetTest, print_locations
 import pysopnet as ps
 
+USE_PARALLEL = False
+
+if USE_PARALLEL:
+	from joblib import Parallel, delayed
+
 # Setup Sopnet environment
 st = SopnetTest()
+st.clear_database(clear_slices=False, clear_segments=False, clear_solutions=True)
 st.setup_sopnet(loglevel=4)
 st.log("Starting blockwise Sopnet")
 
@@ -21,17 +27,26 @@ solutionGuarantorParameters = ps.SolutionGuarantorParameters()
 solutionGuarantorParameters.setCorePadding(1)
 solutionGuarantorParameters.setForceExplanation(True)
 
-sg = ps.SolutionGuarantor()
+solutionGuarantor = ps.SolutionGuarantor()
+
+jobs = []
+
+def fill_core(x, y, z):
+	request = ps.point3(x, y, z)
+	print "Issuing first request for core (%s,%s,%s)" % (x, y, z)
+	missing = solutionGuarantor.fill(request, solutionGuarantorParameters, config)
+
+	if len(missing) > 0:
+		raise "There are (at least) the following segments missing: " + str(missing)
 
 bi = BlockInfo.objects.get(configuration_id=st.segmentation_configuration_id)
 for i in range(0, bi.num_x/bi.core_dim_x):
 	for j in range(0, bi.num_y/bi.core_dim_y):
 		for k in range(0, bi.num_z/bi.core_dim_z):
-			request = ps.point3(i,j,k)
+			if USE_PARALLEL:
+				jobs.append(delayed(fill_core)(i, j, k))
+			else:
+				fill_core(i, j, k)
 
-			print "Issuing first request for core (%s,%s,%s)" % (request.x,request.y,request.z)
-
-			missing = sg.fill(request, solutionGuarantorParameters, config)
-
-			if len(missing) > 0:
-				raise "There are (at least) the following segments missing: " + str(missing)
+if USE_PARALLEL:
+	Parallel(n_jobs=8)(jobs)
