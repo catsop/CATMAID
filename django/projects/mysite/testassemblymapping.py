68f34137c50eecc494f372d9b08e9af9fe9826ce
Andrew Champion <andrew.champion@gmail.com>
2016-04-28 16:58:27 -0400

    Djsopnet: make assembly test script parallel

--- django/projects/mysite/testassemblymapping.py
+++ django/projects/mysite/testassemblymapping.py
@@ -1,31 +1,37 @@
-import sys, os
-sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-from django.conf import settings
+import os
 
-from django.contrib.auth.models import User
-from djsopnet.control.assembly import *
-from djsopnet.control.assembly import \
-	_map_assembly_equivalence_to_skeleton
-from djsopnet.models import BlockInfo, SegmentationConfiguration, SegmentationStack
-from djsopnet.control.block import _blockcursor_to_namedtuple
-from tests.testsopnet import SopnetTest
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
 from collections import namedtuple
 
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import connection
+
+from djsopnet.control.assembly import \
+	generate_assembly_equivalences, \
+	generate_compatible_assemblies_between_cores, \
+	_map_assembly_equivalence_to_skeleton
+from djsopnet.models import SegmentationConfiguration
+from djsopnet.control.block import _blockcursor_to_namedtuple
+from tests.testsopnet import SopnetTest
+
+PARALLEL_JOBS = getattr(settings, 'SOPNET_TEST_PARALLEL_JOBS', {'ASSEMBLY': False})['ASSEMBLY']
+
+if PARALLEL_JOBS:
+	from joblib import Parallel, delayed
+
 st = SopnetTest()
-Reqfake = namedtuple('Reqfake', ['user', 'project_id'])
-u = User.objects.get(username='drew')
 sc = SegmentationConfiguration.objects.get(pk=st.segmentation_configuration_id)
-request = Reqfake(user=u, project_id=sc.project_id)
 segstack = sc.segmentationstack_set.get(type='Membrane')
-
-cursor = connection.cursor()
-
-# Generate assembly compatibility edges for all (6-)neighboring, solved cores.
 bi = sc.block_info
 block_size = bi.size_for_unit('block')
-for (i, j, k) in bi.core_range():
+
+jobs = []
+
+# Generate assembly compatibility edges for all (6-)neighboring, solved cores.
+def core_compatibility(i, j, k):
+	cursor = connection.cursor()
 	cursor.execute('''
 		SELECT * FROM segstack_%s.core
 		WHERE coordinate_x = %s
@@ -49,19 +55,48 @@ for (i, j, k) in bi.core_range():
 				if nbr.solution_set_flag:
 					generate_compatible_assemblies_between_cores(segstack.id, c.id, nbr.id)
 
+for core_coord in bi.core_range():
+	if PARALLEL_JOBS:
+		connection.close()
+		jobs.append(delayed(core_compatibility)(*core_coord))
+	else:
+		core_compatibility(*core_coord)
+
+if PARALLEL_JOBS:
+	Parallel(n_jobs=PARALLEL_JOBS)(jobs)
+
+
 # Generate assembly equivalences.
 print 'Generating assembly equivalences...'
 generate_assembly_equivalences(segstack.id)
 
+
 # For each assembly equivalence, map to a skeleton.
-cursor.execute('''
-	SELECT id FROM segstack_%s.assembly_equivalence WHERE skeleton_id IS NULL
-	''' % segstack.id)
-equivalence_ids = [r[0] for r in cursor.fetchall()]
-for equivalence_id in equivalence_ids:
+Reqfake = namedtuple('Reqfake', ['user', 'project_id'])
+u = User.objects.get(username='drew')
+request = Reqfake(user=u, project_id=sc.project_id)
+
+def map_skeleton(equivalence_id):
 	print 'Mapping assembly equivalence %s' % equivalence_id
 	try:
 		_map_assembly_equivalence_to_skeleton(request, segstack.id, equivalence_id)
 	except Exception as e:
 		print '...error'
 		print str(e)
+
+global_cursor = connection.cursor()
+global_cursor.execute('''
+	SELECT id FROM segstack_%s.assembly_equivalence WHERE skeleton_id IS NULL
+	''' % segstack.id)
+equivalence_ids = [r[0] for r in global_cursor.fetchall()]
+
+jobs = []
+for equivalence_id in equivalence_ids:
+	if PARALLEL_JOBS:
+		connection.close()
+		jobs.append(delayed(map_skeleton)(equivalence_id))
+	else:
+		map_skeleton(equivalence_id)
+
+if PARALLEL_JOBS:
+	Parallel(n_jobs=PARALLEL_JOBS)(jobs)
